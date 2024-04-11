404620cb792e00f1a9a5c844395e4186969b1e81
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2024-04-11 12:25:56 +0530

    added app name wiki

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-    
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>",views.entry_page, name="title"),
